452e141e68889bfa57751f7d3f283097f6b27e5f
melissaintrees <melissaintrees@gmail.com>
2017-12-11 17:47:44 -0600

    add server and paths to public html pages

--- server.js
+++ server.js
@@ -1,3 +1,27 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/home", function(req, res) {
+  res.sendFile(path.join(__dirname, "app/public/home.html"));
+});
+
+app.get("/survey", function(req, res) {
+  res.sendFile(path.join(__dirname, "app/public/survey.html"));
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
