8b3566aba2d0eff6d2eb8694cbf288207a2e3244
melissaintrees <melissaintrees@gmail.com>
2017-12-12 17:45:27 -0600

    add wild card route, and jquery event listeners

---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+// console.log("dog");
+
+$("#formSubmit").click(function(events) {
+  events.preventDefault();
+  // console.log("cat");
+  var answerOne = $("#questionOne").val();
+  console.log("answer one: " + answerOne);
+
+  var answerTwo = $("#questionTwo").val();
+  console.log("answer Two: " + answerTwo);
+
+  var answerThree = $("#questionThree").val();
+  console.log("answer Three: " + answerThree);
+
+  var answerFour = $("#questionFour").val();
+  console.log("answer Four: " + answerFour);
+
+  var answerFive = $("#questionFive").val();
+  console.log("answer Five: " + answerFive);
+
+  var answerSix = $("#questionSix").val();
+  console.log("answer Six: " + answerSix);
+
+  var answerSeven = $("#questionSeven").val();
+  console.log("answer Seven: " + answerSeven);
+
+  var answerEight = $("#questionEight").val();
+  console.log("answer Eight: " + answerEight);
+
+  var answerNine = $("#questionNine").val();
+  console.log("answer Nine: " + answerNine);
+
+  var answerTen = $("#questionTen").val();
+  console.log("answer Ten: " + answerTen);
+});
--- server.js
+++ server.js
@@ -1,27 +1,41 @@
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
+
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path");
 
-// Sets up the Express App
-// =============================================================
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+// Tells node that we are creating an "express" server
 var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
 var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Basic route that sends the user first to the AJAX Page
-app.get("/home", function(req, res) {
-  res.sendFile(path.join(__dirname, "app/public/home.html"));
-});
+app.use(express.static("app/data"));
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
 
-app.get("/survey", function(req, res) {
-  res.sendFile(path.join(__dirname, "app/public/survey.html"));
-});
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
 
-// Starts the server to begin listening
-// =============================================================
 app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+  console.log("App listening on PORT: " + PORT);
 });
+
