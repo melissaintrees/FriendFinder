51367df251652c10e93c3f70804b2dd194ee3db9
melissaintrees <melissaintrees@gmail.com>
2017-12-13 13:56:43 -0600

    clean form fields in survey.html, add jquery clear form in friends.js, and push new objects to array call friendsMarchArr

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,35 +1,104 @@
 // console.log("dog");
+// initialize an empty array for holding the object:
+var friendsMatchArr = [];
+
 
 $("#formSubmit").click(function(events) {
   events.preventDefault();
-  // console.log("cat");
-  var answerOne = $("#questionOne").val();
-  console.log("answer one: " + answerOne);
 
-  var answerTwo = $("#questionTwo").val();
-  console.log("answer Two: " + answerTwo);
+// Create newUser data from user input
+      var newUser = {
+        nameInput: $("#nameInput").val().trim(),
+        picInput: $("#picInput").val().trim(),
+        surveyScores: [
+          $("#questionOne").val(),
+          $("#questionTwo").val(),
+          $("#questionThree").val(),
+          $("#questionFour").val(),
+          $("#questionFive").val(),
+          $("#questionSix").val(),
+          $("#questionSeven").val(),
+          $("#questionEight").val(),
+          $("#questionNine").val(),
+          $("#questionTen").val()
+        ]
+      };
+      console.log("User Input: ", newUser);
+      // push the newUser objects into an JSON array:
+          friendsMatchArr.push(
+            newUser
+          );
+          console.log(JSON.stringify(friendsMatchArr));
+          // clear the values in the form fields:
+          $("#nameInput").val(""),
+          $("#picInput").val(""),
+          $("#questionOne").val(""),
+          $("#questionTwo").val(""),
+          $("#questionThree").val(""),
+          $("#questionFour").val(""),
+          $("#questionFive").val(""),
+          $("#questionSix").val(""),
+          $("#questionSeven").val(""),
+          $("#questionEight").val(""),
+          $("#questionNine").val(""),
+          $("#questionTen").val("")
+  });
 
-  var answerThree = $("#questionThree").val();
-  console.log("answer Three: " + answerThree);
 
-  var answerFour = $("#questionFour").val();
-  console.log("answer Four: " + answerFour);
 
-  var answerFive = $("#questionFive").val();
-  console.log("answer Five: " + answerFive);
 
-  var answerSix = $("#questionSix").val();
-  console.log("answer Six: " + answerSix);
 
-  var answerSeven = $("#questionSeven").val();
-  console.log("answer Seven: " + answerSeven);
 
-  var answerEight = $("#questionEight").val();
-  console.log("answer Eight: " + answerEight);
 
-  var answerNine = $("#questionNine").val();
-  console.log("answer Nine: " + answerNine);
 
-  var answerTen = $("#questionTen").val();
-  console.log("answer Ten: " + answerTen);
-});
+
+
+
+
+
+
+
+
+
+
+
+// $("#formSubmit").click(function(events) {
+//   events.preventDefault();
+//   // console.log("cat");
+//   var nameInput = $("#nameInput").val();
+//   console.log("friend Name: " + nameInput);
+
+//   var picInput = $("#picInput").val();
+//   console.log("photo link: " + picInput);
+
+//   var answerOne = $("#questionOne").val();
+//   console.log("answer one: " + answerOne);
+
+//   var answerTwo = $("#questionTwo").val();
+//   console.log("answer Two: " + answerTwo);
+
+//   var answerThree = $("#questionThree").val();
+//   console.log("answer Three: " + answerThree);
+
+//   var answerFour = $("#questionFour").val();
+//   console.log("answer Four: " + answerFour);
+
+//   var answerFive = $("#questionFive").val();
+//   console.log("answer Five: " + answerFive);
+
+//   var answerSix = $("#questionSix").val();
+//   console.log("answer Six: " + answerSix);
+
+//   var answerSeven = $("#questionSeven").val();
+//   console.log("answer Seven: " + answerSeven);
+
+//   var answerEight = $("#questionEight").val();
+//   console.log("answer Eight: " + answerEight);
+
+//   var answerNine = $("#questionNine").val();
+//   console.log("answer Nine: " + answerNine);
+
+//   var answerTen = $("#questionTen").val();
+//   console.log("answer Ten: " + answerTen);
+// });
+
