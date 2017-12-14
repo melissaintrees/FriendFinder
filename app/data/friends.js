56546b3bd71d2524c8569549cdbafbfeed4596c1
melissaintrees <melissaintrees@gmail.com>
2017-12-13 19:17:16 -0600

    updated the friends.js file to include an array of made up friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,104 +1,56 @@
-// console.log("dog");
-// initialize an empty array for holding the object:
-var friendsMatchArr = [];
 
+// make a friends array of dummy data to match with user inputs
 
-$("#formSubmit").click(function(events) {
-  events.preventDefault();
-
-// Create newUser data from user input
-      var newUser = {
-        nameInput: $("#nameInput").val().trim(),
-        picInput: $("#picInput").val().trim(),
-        surveyScores: [
-          $("#questionOne").val(),
-          $("#questionTwo").val(),
-          $("#questionThree").val(),
-          $("#questionFour").val(),
-          $("#questionFive").val(),
-          $("#questionSix").val(),
-          $("#questionSeven").val(),
-          $("#questionEight").val(),
-          $("#questionNine").val(),
-          $("#questionTen").val()
-        ]
-      };
-      console.log("User Input: ", newUser);
-      // push the newUser objects into an JSON array:
-          friendsMatchArr.push(
-            newUser
-          );
-          console.log(JSON.stringify(friendsMatchArr));
-          // clear the values in the form fields:
-          $("#nameInput").val(""),
-          $("#picInput").val(""),
-          $("#questionOne").val(""),
-          $("#questionTwo").val(""),
-          $("#questionThree").val(""),
-          $("#questionFour").val(""),
-          $("#questionFive").val(""),
-          $("#questionSix").val(""),
-          $("#questionSeven").val(""),
-          $("#questionEight").val(""),
-          $("#questionNine").val(""),
-          $("#questionTen").val("")
-  });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+var friends = [
+  {
+    "nameInput": "Griselda",
+    "picInput":"http://lorempixel.com/400/200/people",
+    "surveyScores": [5, 2, 4, 3, 1, 5, 5, 1, 4, 5]
+  }
+];
 
 
 // $("#formSubmit").click(function(events) {
 //   events.preventDefault();
-//   // console.log("cat");
-//   var nameInput = $("#nameInput").val();
-//   console.log("friend Name: " + nameInput);
 
-//   var picInput = $("#picInput").val();
-//   console.log("photo link: " + picInput);
+// // Create newUser data from user input
+//       var newUser = {
+//         nameInput: $("#nameInput").val().trim(),
+//         picInput: $("#picInput").val().trim(),
+//         surveyScores: [
+//           $("#questionOne").val(),
+//           $("#questionTwo").val(),
+//           $("#questionThree").val(),
+//           $("#questionFour").val(),
+//           $("#questionFive").val(),
+//           $("#questionSix").val(),
+//           $("#questionSeven").val(),
+//           $("#questionEight").val(),
+//           $("#questionNine").val(),
+//           $("#questionTen").val()
+//         ]
+//       };
+//       console.log("User Input: ", newUser);
+//       // push the newUser objects into an JSON array:
+//           friendsMatchArr.push(
+//             newUser
+//           );
+//           console.log(JSON.stringify(friendsMatchArr));
 
-//   var answerOne = $("#questionOne").val();
-//   console.log("answer one: " + answerOne);
+//           // clear the values in the form fields:
+//           $("#nameInput").val(""),
+//           $("#picInput").val(""),
+//           $("#questionOne").val(""),
+//           $("#questionTwo").val(""),
+//           $("#questionThree").val(""),
+//           $("#questionFour").val(""),
+//           $("#questionFive").val(""),
+//           $("#questionSix").val(""),
+//           $("#questionSeven").val(""),
+//           $("#questionEight").val(""),
+//           $("#questionNine").val(""),
+//           $("#questionTen").val("")
+//   });
 
-//   var answerTwo = $("#questionTwo").val();
-//   console.log("answer Two: " + answerTwo);
 
-//   var answerThree = $("#questionThree").val();
-//   console.log("answer Three: " + answerThree);
-
-//   var answerFour = $("#questionFour").val();
-//   console.log("answer Four: " + answerFour);
-
-//   var answerFive = $("#questionFive").val();
-//   console.log("answer Five: " + answerFive);
-
-//   var answerSix = $("#questionSix").val();
-//   console.log("answer Six: " + answerSix);
-
-//   var answerSeven = $("#questionSeven").val();
-//   console.log("answer Seven: " + answerSeven);
-
-//   var answerEight = $("#questionEight").val();
-//   console.log("answer Eight: " + answerEight);
-
-//   var answerNine = $("#questionNine").val();
-//   console.log("answer Nine: " + answerNine);
-
-//   var answerTen = $("#questionTen").val();
-//   console.log("answer Ten: " + answerTen);
-// });
-
+module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,6 +3,8 @@
 // We need to include the path package to get the correct file path for our html
 // ===============================================================================
 var path = require("path");
+var friends = require('../data/friends.js');
+
 
 module.exports = function(app) {
 
@@ -10,6 +12,7 @@ module.exports = function(app) {
   // A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
   app.get("/api/friends", function(req, res) {
     res.sendFile(path.join(__dirname, "../data/friends.js"));
+    res.json(friends);
   });
 
   // A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
