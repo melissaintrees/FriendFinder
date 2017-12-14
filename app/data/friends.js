8f6b148d22971b3943bf1fd53cd43e9c687ff044
melissaintrees <melissaintrees@gmail.com>
2017-12-13 19:22:24 -0600

    add more dummy data

--- app/data/friends.js
+++ app/data/friends.js
@@ -6,6 +6,31 @@ var friends = [
     "nameInput": "Griselda",
     "picInput":"http://lorempixel.com/400/200/people",
     "surveyScores": [5, 2, 4, 3, 1, 5, 5, 1, 4, 5]
+  },
+  {
+    "nameInput": "Don",
+    "picInput":"http://lorempixel.com/400/200/people",
+    "surveyScores": [2, 1, 1, 1, 1, 1, 1, 1, 3, 3]
+  },
+  {
+    "nameInput": "George",
+    "picInput":"http://lorempixel.com/400/200/people",
+    "surveyScores": [1, 2, 2, 2, 1, 3, 3, 5, 3, 5]
+  },
+  {
+    "nameInput": "Hazelnut",
+    "picInput":"http://lorempixel.com/400/200/people",
+    "surveyScores": [2, 1, 1, 5, 5, 5, 4, 3, 1, 1]
+  },
+  {
+    "nameInput": "Bean",
+    "picInput":"http://lorempixel.com/400/200/people",
+    "surveyScores": [1, 2, 3, 4, 5, 4, 5, 3, 2, 1]
+  },
+  {
+    "nameInput": "Americao",
+    "picInput":"http://lorempixel.com/400/200/people",
+    "surveyScores": [1, 1, 1, 1, 2, 3, 4, 3, 2, 5]
   }
 ];
 
