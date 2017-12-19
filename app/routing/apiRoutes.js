8721979d73e6a1ecc3daf10f1fdbecd4ddf8cc87
melissaintrees <melissaintrees@gmail.com>
2017-12-18 22:18:14 -0600

    add logic to POST request to for loop through friends array and get object that matches user search input

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,14 +11,36 @@ module.exports = function(app) {
 
   // A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
   app.get("/api/friends", function(req, res) {
+    // this routes the initial friends (what is in the friends.js) array to the api/friends path:
+    console.log("This is the response: ", res);
     res.json(friends);
+
+  });
+
+  app.post("/api/getfriendname", function(req, res) {
+
+    //req.body = {nameInput: userEntry}
+    var UserName = req.body.nameInput
+    var foundUser;
+
+    for (var i = 0; i < friends.length; i ++){
+        if (UserName === friends[i].nameInput) {
+          foundUser = friends[i];
+          break
+        }
+      };
+      if (foundUser) {
+        return res.json(foundUser);
+    } else {
+      return res.json({error: "sorry, this user is not in the system"})
+    }
   });
 
   // A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
 
   app.post("/api/friends", function(req, res) {
     console.log("This is the request from the survey: ", req.body)
-
+    // req.body(friends);
   });
 
 
