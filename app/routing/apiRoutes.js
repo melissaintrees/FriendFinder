8fe37a906d041d2326b13ca824a1e8c850a46761
melissaintrees <melissaintrees@gmail.com>
2018-01-16 14:43:15 -0600

    push new USer data from req.body to freinds array

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,40 +9,38 @@ var friends = require('../data/friends.js');
 module.exports = function(app) {
 
 
-  // A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
+  // A GET route with the url `/api/friends`. returns friends
   app.get("/api/friends", function(req, res) {
     // this routes the initial friends (what is in the friends.js) array to the api/friends path:
-    console.log("This is the response: ", res);
+    // console.log("This is the response: ", res);
     res.json(friends);
-
   });
 
-  app.post("/api/getfriendname", function(req, res) {
-
-    // req.body = {nameInput: userEntry}
-    // get the request from the friends object.nameInput back and assign it to the User Name var?
-    var UserName = req.body.surveyScores
-    var foundUser;
-
-    for (var i = 0; i < friends.length; i ++){
-        if (UserName === friends[i].surveyScores) {
-          foundUser = friends[i];
-          break
-        }
-      };
-      if (foundUser) {
-        return res.json(foundUser);
-    } else {
-      return res.json({error: "sorry, this user is not in the system"})
-    }
-  });
-
-  // A POST routes `/api/friends`. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
 
   app.post("/api/friends", function(req, res) {
-    console.log("This is the request from the survey: ", req.body)
-    // req.body(friends);
+
+    // console.log("This is the request: ", req);
+    // store the req object in a var called UserInput:
+    var userInput = req.body;
+    console.log("this is in the userInput var: ", userInput);
+    var foundUser;
+    // i want to push the new UserInput to the friends Object and it works!:
+    friends.push(userInput);
+
   });
 
-
 };
+
+// old code dude
+
+    // for (var i = 0; i < friends.length; i ++){
+    //     if (UserName === friends[i].surveyScores) {
+    //       foundUser = friends[i];
+    //       break
+    //     }
+    //   };
+    //   if (foundUser) {
+    //     return res.json(foundUser);
+    // } else {
+    //   return res.json({error: "sorry, this user is not in the system"})
+    // }
