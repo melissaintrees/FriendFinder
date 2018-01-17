2fa5a2d41916dcb5851f7c71d344a51e89dada12
melissaintrees <melissaintrees@gmail.com>
2018-01-17 14:35:02 -0600

    Add logic in api routes to calculate best match

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -16,30 +16,78 @@ module.exports = function(app) {
      res.json(friends);
   });
 
-
+// POST route for /api/friends that takes in the new user data and responds with the most compatible match.
   app.post("/api/friends", function(req, res) {
 
     // console.log("This is the request: ", req);
     // store the req object in a var called UserInput:
     var userInput = req.body;
+    var differences = [];
 
+  // If there are more then one friends to compare to, do this:
+      if (friends.length > 1) {
+        // Step through these potential friends.
+        friends.forEach(function(user) {
+          var totalDifference = 0;
 
-    // console.log("this is in the userInput var: ", userInput);
-    var newUserScores = userInput.surveyScores;
-    console.log("number array for the newUser Scores: ", newUserScores);
+          // For each answer, compare the surveyScores and add the absolute value of the difference to the total difference.
+          for (var i = 0; i < userInput.surveyScores.length; i++) {
+              var otherAnswer = user.surveyScores[i];
+              var thisAnswer = userInput.surveyScores[i];
+              var difference = +otherAnswer - +thisAnswer;
+              totalDifference += Math.abs(difference);
+          }
+          differences.push(totalDifference);
+        });
+       // Find the minimum difference score.
+        var minimumDifference = Math.min.apply(null, differences);
+       // Since there may be more than one potential friend with that score, create an array.
+        var bestMatches = [];
+       // For each item in differences, if it is equal to the minimumDifference, add the corresponding friendData to the bestMatches array.
+        for (var i = 0; i < differences.length; i++) {
+          if (differences[i] === minimumDifference) {
+              bestMatches.push(friends[i]);
+          }
+        }
 
-    // Convert new user's results into a simple array of numbers:
-    // Iterate over the friends array to get a number array of scores for every user in the friends array
+        // Then send bestMatches to the client.
+        console.log(bestMatches);
+        res.json(bestMatches);
 
-    // Convert each user's results into a simple array of numbers:
-    var surveySco = friends[0].surveyScores;
-    console.log("this is the surveyScore from all friends: ", surveySco);
-    // i want to push the new UserInput to the friends Object and it works!:
-    // friends.push(userInput);
-
+        // If there is only one friend to compare to, skip all that work and just send back that friend.
+        } else {
+            res.json(friends);
+        }
+        // if only one friend to compare to push the UserInput to the friends Object and only send back one friend:
+        friends.push(userInput);
   });
-
 };
+
+//     // console.log("this is in the userInput var: ", userInput);
+//     var newUserScores = userInput.surveyScores;
+//     console.log("this is new user scores: ", newUserScores);
+
+
+//     // Convert new user's results into a simple array of numbers:
+//     // Iterate over the friends array to get a number array of scores for every user in the friends array
+
+//     // Convert each user's results into a simple array of numbers:
+
+//     for (var i = 0; i < friends.length; i++){
+//           surveySco = friends[i].surveyScores;
+//            console.log("this is the surveyScore from", friends[i].nameInput, ": ", surveySco);
+//            console.log("this is the survey score from", userInput.nameInput, ": ", newUserScores);
+
+
+//     }
+//     // var surveySco = friends[0].surveyScores;
+
+//     // i want to push the new UserInput to the friends Object and it works!:
+//     // friends.push(userInput);
+
+//   });
+
+// };
 
 // old code dude
 
