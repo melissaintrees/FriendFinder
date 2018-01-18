36194a829355bce785869e774d816f63c887cca6
melissaintrees <melissaintrees@gmail.com>
2018-01-18 14:07:01 -0600

    add gifs and pics to dummy data

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,33 +3,38 @@
 
 var friends = [
   {
-    "nameInput": "Griselda",
-    "picInput":"http://lorempixel.com/400/200/people",
+    "nameInput": "Nicki",
+    "picInput":"https://media.giphy.com/media/13G4ymdBlMJQis/giphy.gif",
     "surveyScores": [5, 2, 4, 3, 1, 5, 5, 1, 4, 5]
   },
   {
-    "nameInput": "Don",
-    "picInput":"http://lorempixel.com/400/200/people",
+    "nameInput": "Melissa",
+    "picInput":"https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAApNAAAAJDdkOTk4OWE4LWEyZWUtNDgxMS04YjFjLTJkYjAyNmJkOTViOQ.jpg",
+    "surveyScores": [5, 4, 4, 5, 5, 5, 5, 1, 3, 5]
+  },
+  {
+    "nameInput": "Weezy",
+    "picInput":"https://media.giphy.com/media/13EKuSILwlL25G/giphy.gif",
     "surveyScores": [2, 1, 1, 1, 1, 1, 1, 1, 3, 3]
   },
   {
-    "nameInput": "George",
-    "picInput":"http://lorempixel.com/400/200/people",
+    "nameInput": "Ariel",
+    "picInput":"https://media.giphy.com/media/KbcHdem1eDw64/giphy.gif",
     "surveyScores": [1, 2, 2, 2, 1, 3, 3, 5, 3, 5]
   },
   {
-    "nameInput": "Hazelnut",
-    "picInput":"http://lorempixel.com/400/200/people",
+    "nameInput": "Kimmy",
+    "picInput":"https://media.giphy.com/media/l46Cz3nhIKEQYwBK8/giphy.gif",
     "surveyScores": [2, 1, 1, 5, 5, 5, 4, 3, 1, 1]
   },
   {
-    "nameInput": "Bean",
-    "picInput":"http://lorempixel.com/400/200/people",
+    "nameInput": "Mr. Bean",
+    "picInput":"https://media.giphy.com/media/9eSJ3YKbFDtS0/giphy.gif",
     "surveyScores": [1, 2, 3, 4, 5, 4, 5, 3, 2, 1]
   },
   {
-    "nameInput": "Americao",
-    "picInput":"http://lorempixel.com/400/200/people",
+    "nameInput": "The Log Lady",
+    "picInput":"https://media.giphy.com/media/T2VCIPbVrr5ks/giphy.gif",
     "surveyScores": [1, 1, 1, 1, 2, 3, 4, 3, 2, 5]
   }
 ];
