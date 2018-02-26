3366031aed252598d9e42eb97c40a24097cb8ae7
melissaintrees <melissaintrees@gmail.com>
2018-02-25 21:05:05 -0600

    add friends to the array and refine questions

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,37 +5,52 @@ var friends = [
   {
     "nameInput": "Nicki",
     "picInput":"https://media.giphy.com/media/13G4ymdBlMJQis/giphy.gif",
-    "surveyScores": [5, 2, 4, 3, 1, 5, 5, 1, 4, 5]
+    "surveyScores": [5, 4, 5, 1, 3, 2, 1, 3, 2, 5]
   },
   {
     "nameInput": "Melissa",
     "picInput":"https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAApNAAAAJDdkOTk4OWE4LWEyZWUtNDgxMS04YjFjLTJkYjAyNmJkOTViOQ.jpg",
-    "surveyScores": [5, 4, 4, 5, 5, 5, 5, 1, 3, 5]
+    "surveyScores": [1, 5, 4, 1, 2, 3, 4, 1, 5, 4]
   },
   {
-    "nameInput": "Weezy",
-    "picInput":"https://media.giphy.com/media/13EKuSILwlL25G/giphy.gif",
-    "surveyScores": [2, 1, 1, 1, 1, 1, 1, 1, 3, 3]
+    "nameInput": "Illana",
+    "picInput":"https://media.giphy.com/media/3osxYdZN9vCr0l7AiY/giphy.gif",
+    "surveyScores": [5, 3, 5, 1, 2, 1, 5, 5, 3, 1]
   },
   {
     "nameInput": "Ariel",
     "picInput":"https://media.giphy.com/media/KbcHdem1eDw64/giphy.gif",
-    "surveyScores": [1, 2, 2, 2, 1, 3, 3, 5, 3, 5]
+    "surveyScores": [5, 2, 1, 2, 4, 2, 5, 4, 5, 3]
   },
   {
     "nameInput": "Kimmy",
     "picInput":"https://media.giphy.com/media/l46Cz3nhIKEQYwBK8/giphy.gif",
-    "surveyScores": [2, 1, 1, 5, 5, 5, 4, 3, 1, 1]
+    "surveyScores": [1, 5, 3, 4, 2, 2, 3, 5, 5, 5]
   },
   {
-    "nameInput": "Mr. Bean",
-    "picInput":"https://media.giphy.com/media/9eSJ3YKbFDtS0/giphy.gif",
-    "surveyScores": [1, 2, 3, 4, 5, 4, 5, 3, 2, 1]
+    "nameInput": "Kim",
+    "picInput":"https://media.giphy.com/media/2lG8fMhri8Pmw/giphy.gif",
+    "surveyScores": [2, 4, 1, 5, 5, 2, 1, 1, 4, 1]
   },
   {
     "nameInput": "The Log Lady",
     "picInput":"https://media.giphy.com/media/T2VCIPbVrr5ks/giphy.gif",
-    "surveyScores": [1, 1, 1, 1, 2, 3, 4, 3, 2, 5]
+    "surveyScores": [3, 2, 1, 5, 1, 1, 5, 3, 3, 5]
+  },
+  {
+    "nameInput": "Cher",
+    "picInput": "https://media.giphy.com/media/yj5oYHjoIwv28/giphy.gif",
+    "surveyScores": [3, 5, 5, 1, 5, 5, 1, 1, 4, 2]
+  },
+  {
+    "nameInput": "Elle",
+    "picInput": "https://media.giphy.com/media/AdOQk4qAoPAY0/giphy.gif",
+    "surveyScores": [3, 5, 5, 2, 5, 4, 4, 1, 4, 5]
+  },
+  {
+    "nameInput": "Beyonce",
+    "picInput": "https://media.giphy.com/media/l3V0doGbp2EDaLHJC/giphy.gif",
+    "surveyScores": [2, 4, 3, 2, 3, 5, 2, 1, 3, 4]
   }
 ];
 
